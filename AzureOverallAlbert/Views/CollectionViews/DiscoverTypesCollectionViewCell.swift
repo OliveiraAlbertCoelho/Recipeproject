19fac3ce23b11788a57528d4d05ff7ce1966a9fc
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2020-05-06 13:09:10 -0400

    adds title label to discoverType collection cell

--- AzureOverallAlbert/Views/CollectionViews/DiscoverTypesCollectionViewCell.swift
+++ AzureOverallAlbert/Views/CollectionViews/DiscoverTypesCollectionViewCell.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class DiscoverTypesCollectionViewCell: UICollectionViewCell {
+      //MARK: - Lifecycle
     override init(frame: CGRect) {
          super.init(frame: frame)
+         setUpView()
       }
       
       required init?(coder: NSCoder) {
          fatalError("init(coder:) has not been implemented")
       }
+   //MARK: - Variables
+   //MARK: - UI Objects
+   lazy var titleLabel: UILabel = {
+      let label = UILabel()
+      return label
+   }()
+   //MARK: - Objc Functions
+   //MARK: - Regular Functions
+   private func setUpView(){
+      contentView.backgroundColor = .white
+      constrainTitleLabel()
       
+   }
+   private func constrainTitleLabel(){
+      contentView.addSubview(titleLabel)
+      titleLabel.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+         titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+         titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+         titleLabel.heightAnchor.constraint(equalToConstant: 40),
+         titleLabel.widthAnchor.constraint(equalToConstant: 40)
+      ])
+   }
+   //MARK: - Constraints
 }
--- AzureOverallAlbert/Views/TableViewCells/RecipesTableViewCell.swift
+++ AzureOverallAlbert/Views/TableViewCells/RecipesTableViewCell.swift
@@ -12,6 +12,7 @@ class RecipesTableViewCell: UITableViewCell {
    //MARK: - Lifecycle
       override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
        super.init(style: style, reuseIdentifier: reuseIdentifier)
+         setUpContentView()
     }
     required init(coder aDecoder: NSCoder) {
        fatalError("init(coder:) has not been implemented")
